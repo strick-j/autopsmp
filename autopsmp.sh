439036a2aa4a5cf92befe9bb5057027024f0d5d9
Joe Strickland <strick-j@users.noreply.github.com>
2018-08-02 17:15:01 -0400

    fixed tabs / indents

--- autopsmp.sh
+++ autopsmp.sh
@@ -99,35 +99,35 @@ createuser(){
   fi
 }
 dir_prompt(){
-	print_head "Step 2: Collecting Info"
-	# Prompt for directory info of installation folder, verify directory exists
-	print_info "Requesting installation directory information"
-	read -p 'Please enter the full path for the installation folder directory [e.g. /opt/psmp]: ' foldervar
-	if [[ -d $foldervar ]]; then
-		# Directory present, subfolders have not been verified
-		print_success "Directory information confirmed as: $foldervar"
-	else
-		# Directory not present, allow user to re-enter path or exit
-		print_error "Directory not found, would you like to try again?"
-		select yn in "Yes" "No"; do
-			case $yn in
-				Yes ) dir_prompt; break;;
-				No ) exit 1;; 
-			esac
-		done
-	fi	
+  print_head "Step 2: Collecting Info"
+  # Prompt for directory info of installation folder, verify directory exists
+  print_info "Requesting installation directory information"
+  read -p 'Please enter the full path for the installation folder directory [e.g. /opt/psmp]: ' foldervar
+  if [[ -d $foldervar ]]; then
+    # Directory present, subfolders have not been verified
+    print_success "Directory information confirmed as: $foldervar"
+  else
+    # Directory not present, allow user to re-enter path or exit
+    print_error "Directory not found, would you like to try again?"
+    select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) dir_prompt; break;;
+        No ) exit 1;; 
+      esac
+    done
+  fi	
 }
 info_prompt(){
-	# Prompt for Vault IP and Username info
-	echo
-	print_info "Requesting Vault IP and Username for PSMP Installation"
-	read -p 'Please enter Vault IP Address: ' ipvar
-	read -p 'Please enter Vault Username: ' uservar
+  # Prompt for Vault IP and Username info
+  echo
+  print_info "Requesting Vault IP and Username for PSMP Installation"
+  read -p 'Please enter Vault IP Address: ' ipvar
+  read -p 'Please enter Vault Username: ' uservar
 }
 pass_prompt(){
-	# Prompt for Vault Password info
-	echo 
-	print_info "Requesting Vault Password. Password must be entered twice and will be hidden"
+  # Prompt for Vault Password info
+  echo 
+  print_info "Requesting Vault Password. Password must be entered twice and will be hidden"
   read -sp 'Please enter Vault Password: ' passvar1
   echo
   read -sp 'Please re-enter Vault Password: ' passvar2
@@ -140,31 +140,31 @@ pass_prompt(){
     unset passvar1
   else
     print_error "Passwords do not match. Please try again."
-    pass_prompt 
+    pass_prompt
   fi
 }
 cred_create(){
-	# Create credential file with username and password provided above
-	print_head "Step 3: Modifying Files"
-	print_info "Creating Credential File for authorization to Vault"
-	# Verify CreateCredFile is present, exit if not
-	if [[ -f $foldervar/CreateCredFile ]];then
-		# Modify permissions and create credential file
-		chmod 755 $foldervar/CreateCredFile
-		$foldervar/CreateCredFile $foldervar/user.cred Password -username $uservar -password $tomcatpass >> autopsmp.log 2>&1
-		# Unset password variable
+  # Create credential file with username and password provided above
+  print_head "Step 3: Modifying Files"
+  print_info "Creating Credential File for authorization to Vault"
+  # Verify CreateCredFile is present, exit if not
+  if [[ -f $foldervar/CreateCredFile ]];then
+    # Modify permissions and create credential file
+    chmod 755 $foldervar/CreateCredFile
+    $foldervar/CreateCredFile $foldervar/user.cred Password -username $uservar -password $tomcatpass >> autopsmp.log 2>&1
+    # Unset password variable
     unset tomcatpass
     vercredvar=$(tail -1 autopsmp.log)
-		if [[ "$vercredvar" == "Command ended successfully" ]]; then
-			print_success "Credential file created successfully"
-		else
-			print_error "Credential file not created sueccessfully. Exiting now..."
+    if [[ "$vercredvar" == "Command ended successfully" ]]; then
+      print_success "Credential file created successfully"
+    else
+      print_error "Credential file not created sueccessfully. Exiting now..."
       exit 1
-		fi
-	else
-		print_error "CreateCredFile file not found, verify needed files have been copied over. Exiting now..."
-		exit 1
-	fi
+    fi
+  else
+    print_error "CreateCredFile file not found, verify needed files have been copied over. Exiting now..."
+    exit 1
+  fi
 }
 vaultini_mod(){
   # Modifing vault.ini file with information provided above
@@ -179,14 +179,14 @@ vaultini_mod(){
     # Error - File not found
     print_error "vault.ini file not found, verify needed files have been copied over. Exiting now..."
     exit 1
-	fi
-	print_success "Completed vault.ini file modifications"
+  fi
+  print_success "Completed vault.ini file modifications"
 }
 psmpparms_mod(){
-	# Update psmpparms file with information provided above
-	echo
-	print_info "Updating psmpparms file with user provided information"
-	# Verify psmpparms.sample is present, exit if not
+  # Update psmpparms file with information provided above
+  echo
+  print_info "Updating psmpparms file with user provided information"
+  # Verify psmpparms.sample is present, exit if not
   if [[ -f $foldervar/psmpparms.sample ]]; then
     # Create psmpparms file and modify variables
     cp $foldervar/psmpparms.sample /var/tmp/psmpparms
@@ -200,7 +200,7 @@ psmpparms_mod(){
   print_success "psmpparms file modified and copied to /var/tmp/"
 }
 install_psmp(){
-	# Installing PSMP using rpm file
+  # Installing PSMP using rpm file
   print_head "Step 4: Installing PSMP"
   print_info "Verifying rpm GPG Key is present"
   if [[ -f $foldervar/RPM-GPG-KEY-CyberArk ]]; then
@@ -218,24 +218,24 @@ install_psmp(){
     # Install CyberArk RPM
     print_info "PSMP rpm installer present - installing $psmprpm"
     rpm -ih $foldervar/$psmprpm
-    else
-      # Error - File not found
-      print_error "Necessry rpm installer not found, verify needed files have been copied over. Exiting now..."
-      exit 1
-    fi
+  else
+    # Error - File not found
+    print_error "Necessry rpm installer not found, verify needed files have been copied over. Exiting now..."
+    exit 1
+  fi
 }
 system_cleanup(){
-	# Cleaning up system files used during install
-	print_head "Step 5: System Cleanup"
-	print_info "Removing user.cred, vault.ini, and CreateCredFile Utility"
-	rm -f $foldervar/user.cred
-	rm -f $foldervar/vault.ini
-	rm -f $foldervar/CreateCredFile
-	if [[ -f $foldervar/user.cred ]]||[[ -f $foldervar/vault.ini ]]||[[ -f $foldervar/CreateCredFile ]]; then
-		print_error "Files could not be deleted, please manually remove..."
-	else
-		print_success "System cleanup completed"
-	fi
+  # Cleaning up system files used during install
+  print_head "Step 5: System Cleanup"
+  print_info "Removing user.cred, vault.ini, and CreateCredFile Utility"
+  rm -f $foldervar/user.cred
+  rm -f $foldervar/vault.ini
+  rm -f $foldervar/CreateCredFile
+  if [[ -f $foldervar/user.cred ]]||[[ -f $foldervar/vault.ini ]]||[[ -f $foldervar/CreateCredFile ]]; then
+    print_error "Files could not be deleted, please manually remove..."
+  else
+    print_success "System cleanup completed"
+  fi
   echo ""
 }
 main
