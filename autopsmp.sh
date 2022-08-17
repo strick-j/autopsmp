2d420e12341abd12c97e95c339c7b050d10ead09
Joe Strickland <strick-j@users.noreply.github.com>
2022-08-17 16:02:12 -0400

    Installed bats

--- autopsmp.sh
+++ autopsmp.sh
@@ -433,4 +433,14 @@ system_cleanup(){
   fi
   echo ""
 }
-main
+
+# Check if the function exists (bash specific)
+if declare -f "$1" > /dev/null
+then
+  # call arguments verbatim
+  "$@"
+else
+  # Show a helpful error
+  echo "'$1' is not a known function name" >&2
+  exit 1
+fi
